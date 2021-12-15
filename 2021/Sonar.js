abd24ac2ca2f7db7f9e2b523f9a6f2ada7deb017
Maria Karlsson <mariahkarlsson_@hotmail.com>
2021-12-15 09:50:22 +0100

    Solution for day one complete

--- 2021/Sonar.js
+++ 2021/Sonar.js
@@ -1,22 +1,52 @@
 var fs = require("fs");
 
 var content = fs
-  .readFileSync("./sonar-input.txt", "utf-8", (err) => {
+  .readFileSync("sonar-input.txt", "utf-8", (err) => {
     if (err) return err;
   })
-  .split("\n");
+  .split("\n")
+  .map(Number);
 
-let counter = 1;
+let example = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
 
-for (let i = 0; i < content.length; i++) {
-  let item = content[i];
-  let next = content[i + 1];
+// part 1
+const compareDepthMeasurment = (allDepths) => {
+  let countAllLargerMeasurements = 0;
 
-  if (item < next) {
-    counter++;
-  } else if (item > next) {
-    next = item;
-  }
-}
+  allDepths.map((measurment, i) => {
+    if (measurment > allDepths[i - 1]) {
+      countAllLargerMeasurements++;
+    }
+  });
 
-console.log(counter);
+  console.log(
+    `There are ${countAllLargerMeasurements} measurements larger than the previous one`
+  );
+  return countAllLargerMeasurements;
+};
+
+compareDepthMeasurment(content);
+
+// // part 2
+const compareSumOfSlidingWindow = (allDepths) => {
+  let sumOfSlidingWindow = 0;
+  let slidingDepthsArray = [];
+
+  allDepths.map((measurement, i) => {
+    if (allDepths[i + 1] && allDepths[i + 2]) {
+      slidingDepthsArray.push(
+        measurement + allDepths[i + 1] + allDepths[i + 2]
+      );
+    }
+    if (slidingDepthsArray[i] > slidingDepthsArray[i - 1]) {
+      sumOfSlidingWindow++;
+    }
+
+    return slidingDepthsArray;
+  });
+  console.log(
+    `There are ${sumOfSlidingWindow} sums larger than the previous one`
+  );
+};
+
+compareSumOfSlidingWindow(content);
